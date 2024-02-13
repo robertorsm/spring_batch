d57c17f97b4b9c039b2862e6b62c4bfb627fd490
robertorsm <sroberto.rsm@gmail.com>
2024-02-13 01:56:28 -0300

    Refatorando projeto em pacotes mais legíveis.

---
+++ src/main/java/com/udemy/springbatch/job/PrimeiroJobBatchConfig.java
@@ -0,0 +1,27 @@
+package com.udemy.springbatch.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class PrimeiroJobBatchConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Bean
+    public Job imprimeOlaJob(Step imprimeOlaStep) {
+        return jobBuilderFactory
+                .get("imprimeOlaJob")
+                .start(imprimeOlaStep)
+                .build();
+    }
+
+
+}
