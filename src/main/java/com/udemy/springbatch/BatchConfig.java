f7b63340a74d16b85cb173b7b763094fdec55f06
robertorsm <sroberto.rsm@gmail.com>
2024-02-12 23:04:47 -0300

    Adicionando dependência do mysql e configurando o banco no application.yml.

--- src/main/java/com/udemy/springbatch/BatchConfig.java
+++ src/main/java/com/udemy/springbatch/BatchConfig.java
@@ -2,12 +2,9 @@ package com.udemy.springbatch;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
